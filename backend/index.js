dfbefbaefe1033e4fb13ea01bba3c48172acf730
d-pande <dhruvpande4@gmail.com>
2023-09-17 15:09:04 -0500

    added movies file and created plotly graph

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,62 @@ import cors from "cors"
 
 const app = express()
 
+import fs from 'fs';
+import csv from 'csv-parser';
+
+// Specify the CSV file path and the word to count
+const csvFilePath = '../movies.csv';
+const wordToCount = 'example';
+
+var yearCounts = {}; //stores year counts after being parsed from movies.csv
+var graphData = {
+    type: 'bar'
+};  //arg for plotly graph
+
+// Create a read stream to parse the CSV file
+fs.createReadStream(csvFilePath)
+  .pipe(csv())
+  .on('data', (row) => {
+    var year = row['year'];
+
+    // if it has '(', only keep last 4 chars
+    if (year.includes('(')) {
+        year = year.slice(-4);
+    }
+
+    if( !(year in yearCounts) ) {
+        yearCounts[year] = 1;
+    } else {
+        yearCounts[year]++;
+    }
+    
+  })
+  .on('end', () => {
+    // runs after parsing is finished
+    // console.log(yearCounts);
+    var keysArray = [];
+    var valuesArray = [];
+
+    for (const key in yearCounts) {
+        if (yearCounts.hasOwnProperty(key)) {
+            keysArray.push(key);
+            valuesArray.push(yearCounts[key]);
+        }
+    }
+
+    graphData['x'] = keysArray;
+    graphData['y'] = valuesArray;
+    // console.log(graphData);
+});
+
 app.use(cors({
     origin: "http://localhost:5500"
 }))
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
+    
     res.send({
-        x: [1, 2, 3],
-        y: [4, 5, 6]
+        ...graphData  //... moves attributes from data to res obj
     }).status(200)
 })
 
--- frontend/graph.mjs
+++ frontend/graph.mjs
@@ -1,8 +1,22 @@
 const graphDiv = document.getElementById("graph");
 
-fetch(
-    "https://oa-2023-24-backend.onrender.com" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
-).then(async res => {
-    Plotly.newPlot( graphDiv, [ await res.json() ]); 
-})
+var layout = {
+    title: {
+        text: 'Top 10,000 Movies from IMDB by Year',
+        yref: 'paper',
+        automargin: true,
+    },
+    showlegend: false,
+    xaxis: {title: 'Movie Year'},
+    yaxis: {title: '# of Top Movies'}
+  };
 
+fetch(
+    "http://localhost:3000/api" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+    // "https://oa-2023-24-backend.onrender.com" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+
+    ).then(async res => {
+        let a  =  await res.json()
+        // console.log(a)
+    Plotly.newPlot( graphDiv, [a], layout);  //res.json() is being returned from index.js
+})
